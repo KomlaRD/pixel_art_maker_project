b0ad8adc3816058bd476ef694d8e99c78ca7ca86
komlaRD <ankueric1@gmail.com>
2022-01-11 20:26:38 +0000

    Make grid function with a nested for loop creating rows and cells.

--- designs.js
+++ designs.js
@@ -26,3 +26,21 @@ sizeGrid.addEventListener("submit", function(event) {
     resetGrid();
     makeGrid();
 });
+
+// Make grid function
+
+function makeGrid() {
+    // Nested for loop to create grid with height and width input
+    for (let a = 1; a <= column.value; a++) {
+        //Create table row element and append to table
+        let tableRow = document.createElement("tr");
+        table.appendChild(tableRow);
+        //Create table cells and append to row
+        for (let b = 1; b <= cells.value; b++) {
+            let tableCell = document.createElement("td");
+            tableRow.appendChild(tableCell);
+            // Add color to cells
+            tableCell.addEventListener("click", colorCells);
+        }
+    }
+}
