6e014facdd858ed8566c5c9e56c44382ed8d08d9
komlaRD <ankueric1@gmail.com>
2022-01-11 20:22:50 +0000

    Select height input by id from the html file

--- designs.js
+++ designs.js
@@ -9,3 +9,7 @@ let sizeGrid = document.getElementById("sizePicker");
 // Select table element
 
 let table = document.getElementById("pixelCanvas");
+
+// Select height input
+
+let column = document.getElementById("inputHeight"); // Row variable
